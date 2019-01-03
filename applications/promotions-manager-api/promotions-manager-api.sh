66469a334f7a1da36415955909454ea58760d2b0
alexazarh <aazarh@gmail.com>
2019-01-03 23:51:44 +0200

    fix api init script

--- applications/promotions-manager-api/promotions-manager-api.sh
+++ applications/promotions-manager-api/promotions-manager-api.sh
@@ -11,12 +11,12 @@ echo '==> Extract api artifact to /var/promotions-manager-api'
 mkdir $ARTIFACTS_PATH/drop
 tar -xvf $ARTIFACTS_PATH/promotions-manager-api.*.tar.gz -C $ARTIFACTS_PATH/drop/
 mkdir /var/promotions-manager-api/
-tar -xvf $ARTIFACTS_PATH/drop/drop/promotions-manager-ui.*.tar.gz -C /var/promotions-manager-api
+tar -xvf $ARTIFACTS_PATH/drop/drop/promotions-manager-api.*.tar.gz -C /var/promotions-manager-api
 
 echo '==> Set the DATABASE_HOST env var to be globally available'
-echo 'DATABASE_HOST=$DATABASE_HOST' >> /etc/environment
+echo 'DATABASE_HOST='$DATABASE_HOST >> /etc/environment
 source /etc/environment
 
 echo '==> Install PM2, it provides an easy way to manage and daemonize nodejs applications'
 npm install -g pm2
-$(pm2 startup systemd 2>&1 >/dev/null | grep 'su -c' | sed 's/<user>/root/g' | sed 's/<home-path>//home/root/g/')
+pm2 startup systemd
