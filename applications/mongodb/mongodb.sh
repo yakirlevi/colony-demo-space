e3beaf577f3b0c974a869d53075d0cddab21044f
alexazarh <aazarh@gmail.com>
2019-01-03 22:54:37 +0200

    fixing mongodb init script

--- applications/mongodb/mongodb.sh
+++ applications/mongodb/mongodb.sh
@@ -2,16 +2,22 @@
 echo 'Installing mongodb 4.0'
 
 echo 'Import the Public Key used by the Ubuntu Package Manager'
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
 echo 'Create a file list for mongoDB to fetch the current repository'
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
 echo ' Update the Ubuntu Packages'
-apt update 
+apt-get update 
 
 echo 'Install MongoDB'
-apt install mongodb-org -y
+apt-get install -y mongodb-org
+# prevent auto updates
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
 echo 'Change mongoDB Listening IP Address from local 127.0.0.1 to All IPs 0.0.0.0'
 sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mongod.conf
@@ -19,15 +25,18 @@ sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mongod.conf
 echo 'Start the mongo service'
 service mongod start
 
+echo 'Enable automatically starting MongoDB when the system starts.'
+sudo systemctl enable mongod
+
 echo 'Extracting user data db artifact'
 mkdir $ARTIFACTS_PATH/drop
 tar -xvf $ARTIFACTS_PATH/user-data-db.tar -C $ARTIFACTS_PATH/drop/
+
+echo 'Install mongo clients'
+apt install mongodb-clients -y
 
 echo 'Importing users collection'
 mongoimport --db promo-manager --collection users --file $ARTIFACTS_PATH/drop/users.json
 
 echo 'Importing promotions collection'
 mongoimport --db promo-manager --collection promotions --file $ARTIFACTS_PATH/drop/promotions.json
-
-echo 'Enable automatically starting MongoDB when the system starts.'
-sudo systemctl enable mongod
