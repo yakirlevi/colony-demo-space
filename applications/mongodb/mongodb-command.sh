93072f74be97509edf36e84c16351af622544d99
alexazarh <aazarh@gmail.com>
2019-02-05 09:59:43 +0200

    added support for aks to mongo & api-external apps
    added new blueprint for aks external api testing

--- applications/mongodb/mongodb-command.sh
+++ applications/mongodb/mongodb-command.sh
@@ -2,3 +2,17 @@
 echo 'Run mongodb service'
 # Start the MongoDB Service
 service mongod start
+
+# block while mongod is running
+while true
+do
+  RESULT=`ps -A | sed -n /mongod$/p`
+
+  if [ "${RESULT:-null}" = null ]; then
+    echo "not running"
+    exit 1
+  else
+    echo "running"
+  fi
+  sleep 1
+done
--- applications/promotions-manager-api-external/promotions-manager-api-command.sh
+++ applications/promotions-manager-api-external/promotions-manager-api-command.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo ''
+
+# block while api is running
+while true
+do
+  RESULT=`ps -A | sed -n /index\.js$/p`
+
+  if [ "${RESULT:-null}" = null ]; then
+    echo "not running"
+    exit 1
+  else
+    echo "running"
+  fi
+  sleep 1
+done
