79f86166d9afae2d142954d82f254760cb6a7eef
alexazarh <aazarh@gmail.com>
2019-02-05 16:31:49 +0200

    fixing mongodb for k8s

--- applications/mongodb/mongodb-command-k8s.sh
+++ applications/mongodb/mongodb-command-k8s.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-echo 'Run mongodb service'
-# Start the MongoDB Service
-service mongod start
-
-# block while mongod is running
-while true
-do  
-  sleep 1
-done
+echo ''
---
+++ applications/mongodb/mongodb-k8s.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo 'Running mongodb 4.0'
