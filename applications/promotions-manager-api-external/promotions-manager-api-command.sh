821e50521d13649772ccbc42bb4e63f1e3e1008a
alexazarh <aazarh@gmail.com>
2019-02-05 16:36:36 +0200

    adding a separate command for k8s for api-external app

--- applications/promotions-manager-api-external/promotions-manager-api-command.sh
+++ applications/promotions-manager-api-external/promotions-manager-api-command.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-
-# block while api is running
-while true
-do
-  RESULT=`ps -A | sed -n /index\.js$/p`
-
-  if [ "${RESULT:-null}" = null ]; then
-    echo "not running"
-    exit 1
-  else
-    echo "running"
-  fi
-  sleep 1
-done
+echo ''
