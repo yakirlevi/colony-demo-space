15f01256bdec6489fca4857290678a236a681c46
alexazarh <aazarh@gmail.com>
2019-02-06 16:31:18 +0200

    fixing mongo app init for k8s
    reverting to default HC script for mongo app
    fixing hc script for api-external

--- applications/mongodb/mongodb-k8s.sh
+++ applications/mongodb/mongodb-k8s.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-echo 'Running mongodb 4.0'
 
-echo 'Change mongoDB Listening IP Address from local 127.0.0.1 to All IPs 0.0.0.0'
-sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mongod.conf
-
-echo 'Start the mongo service'
-service mongod start
-
-echo 'Enable automatically starting MongoDB when the system starts.'
-sudo systemctl enable mongod
+# save all env for debugging
+printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 
 echo 'Extracting user data db artifact'
 mkdir $ARTIFACTS_PATH/drop
--- applications/promotions-manager-api-external/promotions-manager-api-hc.sh
+++ applications/promotions-manager-api-external/promotions-manager-api-hc.sh
@@ -6,7 +6,7 @@ hostname=$ip:$API_PORT
 for (( ; ; ))
 do
     sleep 1
-    if [[ $(wget -q --spider -T1 -t1 ${hostname} && echo $?) == "0" ]] ; then
+    if [[ $(wget -S -T1 -t1 http://${hostname}/api/public/version 2>&1 | grep "HTTP/1" | awk '{print $2}') == "200" ]] ; then
         echo 'connected to' ${hostname}
         exit 0
     else
