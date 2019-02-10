a8e085b683a3c9465ea661180f98712cfc42dd4d
alexazarh <aazarh@gmail.com>
2019-02-10 18:12:48 +0200

    debugging ui for k8s

--- applications/promotions-manager-ui/promotions-manager-ui-k8s.sh
+++ applications/promotions-manager-ui/promotions-manager-ui-k8s.sh
@@ -4,15 +4,15 @@ echo '=============== Staring init script for Promotions Manager UI ============
 # save all env for debugging
 printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 
-echo '==> Extract ui artifact to /var/www/promotions-manager/'
-mkdir $ARTIFACTS_PATH/drop
-tar -xvf $ARTIFACTS_PATH/promotions-manager-ui.*.tar.gz -C $ARTIFACTS_PATH/drop/
-mkdir /var/www/promotions-manager/
-tar -xvf $ARTIFACTS_PATH/drop/drop/promotions-manager-ui.*.tar.gz -C /var/www/promotions-manager/
+# echo '==> Extract ui artifact to /var/www/promotions-manager/'
+# mkdir $ARTIFACTS_PATH/drop
+# tar -xvf $ARTIFACTS_PATH/promotions-manager-ui.*.tar.gz -C $ARTIFACTS_PATH/drop/
+# mkdir /var/www/promotions-manager/
+# tar -xvf $ARTIFACTS_PATH/drop/drop/promotions-manager-ui.*.tar.gz -C /var/www/promotions-manager/
 
-echo '==> Configure nginx'
-cd /etc/nginx/conf.d
-cp default.conf default.conf.backup
+# echo '==> Configure nginx'
+# cd /etc/nginx/conf.d
+# cp default.conf default.conf.backup
 
 # cat << EOF > ./default.conf
 # server {
