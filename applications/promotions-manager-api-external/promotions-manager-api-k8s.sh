46cded95f7db903e040004803d2f44a36b97ace6
alexazarh <aazarh@gmail.com>
2020-12-21 21:13:33 -0600

    fixing blueprints following breaking changes in colony integration with k8s

--- applications/promotions-manager-api-external/promotions-manager-api-k8s.sh
+++ applications/promotions-manager-api-external/promotions-manager-api-k8s.sh
@@ -17,7 +17,6 @@ mkdir /var/promotions-manager-api/
 tar -xvf $ARTIFACTS_PATH/drop/drop/promotions-manager-api.*.tar.gz -C /var/promotions-manager-api
 
 echo '==> Set the DATABASE_HOST env var to be globally available'
-DATABASE_HOST=$DATABASE_HOST.$DOMAIN_NAME
 echo 'DATABASE_HOST='$DATABASE_HOST >> /etc/environment
 echo 'RELEASE_NUMBER='$RELEASE_NUMBER >> /etc/environment
 echo 'API_BUILD_NUMBER='$API_BUILD_NUMBER >> /etc/environment
